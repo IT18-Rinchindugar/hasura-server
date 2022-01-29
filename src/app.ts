a24457e24579c1cb04c43b7eb108bb4fbc190ab9
Rinchindugar <rinchindugar70@gmail.com>
2022-01-30 00:15:50 +0800

    refactor: refactor the apollo-server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
-/* eslint-disable no-promise-executor-return */
 /* eslint-disable import/no-extraneous-dependencies */
-
 import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import config from '@lib/config';
-import morgan from 'morgan';
-import winston from '@config/winston';
-import express, { Application, Request, Response } from 'express';
+import express, {
+  Application, Request, Response,
+} from 'express';
 import http from 'http';
 import errorHandler, { notFound } from '@middlewares/error';
 import resolvers from '@appGraphql/resolvers';
@@ -14,11 +12,23 @@ import typeDefs from '@appGraphql/typeDefs';
 
 // Route files
 import { auth } from '@routes';
+import morgan from 'morgan';
 
 const { APP_PORT, NODE_ENV } = config;
 
 export const createApp = async () => {
   const app: Application = express();
+
+  const httpServer = http.createServer(app);
+
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
   // Without this middleware
   app.use(express.json());
@@ -35,28 +45,19 @@ export const createApp = async () => {
   app.use(errorHandler);
 
   // Logging middleware
-  // if (NODE_ENV === 'development') {
-  //   app.use(morgan('dev'));
-  // }
+  if (NODE_ENV === 'development') {
+    const logger = morgan('dev');
 
-  // if (NODE_ENV === 'production') {
-  //   app.use(morgan('combined', { stream: winston.stream }));
-  // }
-
-  const httpServer = http.createServer(app);
-
-  const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-
-  await apolloServer.start();
-  apolloServer.applyMiddleware({ app });
+    httpServer.on('request', (req: Request, res: Response) => {
+      logger(req, res, (err) => {
+        if (err) throw Error();
+      });
+    });
+  }
 
   httpServer.listen(APP_PORT, () => {
     console.log(`🚀 Server running in ${NODE_ENV} mode on port ${APP_PORT}`.green);
-    console.log(`🚀 Apollo Server ready at http://localhost:${APP_PORT}${apolloServer.graphqlPath}`);
+    console.log(`🚀 Apollo Server ready at http://localhost:${APP_PORT}${apolloServer.graphqlPath}`.yellow);
   });
 };
 
